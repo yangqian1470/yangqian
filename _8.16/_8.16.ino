b7054a8acffb18f4ab3fde5b0cdcefc02e7d08ee
yangqian1470 <53455963+yangqian1470@users.noreply.github.com>
2019-09-02 20:42:43 +0800

    lianxian
    添加连线图

--- _8.16/_8.16.ino
+++ _8.16/_8.16.ino
@@ -6,52 +6,30 @@
 #include <Adafruit_SSD1306.h>
 #define OLED_RESET 4
 #define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 26
 #define LOGO16_GLCD_HEIGHT 16
 #define LOGO16_GLCD_WIDTH  16
 Adafruit_SSD1306 display(OLED_RESET);
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000
-};
 
-#if (SSD1306_LCDHEIGHT != 64)
-//#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
 SoftwareSerial mySerial(A2, A0);
 unsigned char Re_buf[8], counter = 0;
 unsigned char sign = 0;
 int i;
 
-/*---------------------------------全局需要的变量---------------------------*/
+/*---------------------------------全局需要的变量-------*/
+int angLAST;
 int x_time1 = 0;
 int y_time1 = 0;
 int x_time0 = 0;
 int y_time0 = 0;
 int x_time = 0;
 int y_time = 0;
-int a = 0;
 int A;
+int a=0;
 float x5 = 0;
 float y5 = 0;
 int x = 0;
 int y = 0;
+int ang8=0;
 float _x = 0;
 float _y = 0;//刷新瞬间两个参数
 float x2 = 0;
@@ -78,11 +56,14 @@ const double wheel_d = 38;//轮直径38mm
 const int x_line = 1000;
 const int y_line = 1000;//光栅线程
 
-void renturnang(int angle);
+void renturnang(int angle8);
 void POS_refresh(int x_step, int y_step, int ang);
 void testscrolltext(int a);
 
 void setup() {
+  pinMode(13,OUTPUT);
+  pinMode(11,INPUT_PULLUP);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   Serial.begin(9600);
   POS_begin();
   GY25begin();
@@ -108,24 +89,13 @@ while (mySerial.available()) {
       ang = (Re_buf[1] << 8 | Re_buf[2]) / 100; //合成数据，去掉小数点后2位
     }
   };
-  
+ //串口事件结束 
   POS_refresh(x_step, y_step, ang);
-  renturnang(ang);
+  renturnang(ang8);
   if (millis() - _millis >= 10)
   {transmission();
   _millis=millis();}
 }
-
-void testscrolltext(int a)
-{
-  display.setTextSize(2);
-  display.setCursor(0, 0);
-  display.clearDisplay();
-  display.println(ang);
-  display.display();
-
-}
-
 
 void blinkX()
 {
@@ -163,26 +133,29 @@ void POS_refresh(int x_step, int y_step, int ang)
   x_time0 = x_step;
   y_time0 = y_step;
   _x = double(x_time) / x_line * PI * wheel_d;
-  _y = double(y_time) / y_line * PI * wheel_d; //行走的距离 单位mm；
+  _y = double(y_time) / y_line * PI * wheel_d; //行走的距离 单位mm
+  if(ang-angLAST<=20 && ang-angLAST>=-20)
+  {
   float a = (-ang - 45) / 180.00 * PI;
+  angLAST=ang;
   y2 = (-_y) * sin(a) + (_x) * cos(a);
   x2 = (_x) * sin(a) +(_y) * cos(a);
-  x5 -= x2;
-  y5 += y2;
-  x = x5;
-  y = y5;
+  x -= x2;
+  y += y2;
+  }
+  ang8=ang;
   x_l = x >> 8;
   x_r = x & 0XFF;
   y_l = y >> 8;
   y_r = y & 0XFF;
 }
 
-void renturnang(int angle)
+void renturnang(int angle8)
 {
-  if (-90 <= angle && angle <= 180)
-    A = angle + 90;
+  if (-90 <= angle8 && angle8 <= 180)
+    A = angle8 + 90;
   else
-    A = angle + 450;
+    A = angle8 + 450;
   A_r = A & 0XFF; //低八位
   A_l = A >> 8; //高八位
 }
@@ -200,24 +173,17 @@ void POS_begin()
 void GY25begin()
 {
   mySerial.begin(115200);//所需软串口波特率
-  delay(1000);
   mySerial.write(0XA5);
   delay(1);
   mySerial.write(0X55);
-  delay(1);
+  delay(1000);
   mySerial.write(0XA5);
   delay(1);
   mySerial.write(0X52);    //初始化GY25,连续输出模式 //初始化GY25,连续输出模式
   delay(1000);
 }
 
-void SSDbegin()
-{
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.display();
-  delay(1000);
-  display.clearDisplay();
-}
+
 void transmission()
 {
 //    Serial.write(0xAA);
@@ -230,8 +196,45 @@ void transmission()
 //    Serial.write(0x55);
     Serial.print("x:");
     Serial.print(x);    
+    Serial.print("  ");
     Serial.print("y:");
     Serial.print(y);   
+    Serial.print("  ");
     Serial.print("p:");
-    Serial.println(ang);
+    Serial.print(ang);
+    Serial.print("  ");
+    Serial.print("ang8:");
+    Serial.print(ang8); 
+    Serial.print("  ");
+    Serial.print("x_step:");
+    Serial.print(x_step); 
+    Serial.print("  ");
+    Serial.print("y_step:");
+    Serial.print(y_step); 
+    Serial.print("  ");
+    Serial.print("y2");
+    Serial.print(y2); 
+    Serial.print("  ");
+    Serial.print("x2:");
+    Serial.println(x2); 
+    display.clearDisplay();   // clears the screen and buffer
+    display.setTextSize(1); //选择字号
+    display.setTextColor(WHITE);  //字体颜色
+    display.setCursor(0,0);   //起点坐标
+    display.print("x:"); 
+    display.println(x);
+    display.print("y:");
+    display.println(y);
+    display.setTextSize(1);
+    display.print("ang:");
+    display.println(ang); 
+    display.print("ang8:");
+    display.println(ang8); 
+    display.print("x_step:");
+    display.print(x_step); 
+    display.print("0000");
+    display.print("y_step:");
+    display.print(y_step); 
+    display.display();
+    delay(1);
 }
